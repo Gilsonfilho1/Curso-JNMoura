755db3e8f021e8a9e5e9178d90ff6dbcd54cec02
Gilson filho <101582105+Gilsonfilho1@users.noreply.github.com>
2024-03-22 12:12:05 -0300

    Add files via upload

--- C#/Basico/Calculadora/Program.cs
+++ C#/Basico/Calculadora/Program.cs
@@ -10,7 +10,7 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            int numero1, numero2, soma;
+            int numero1, numero2, soma, subtracao, divisao, multiplicacao;
 
             Console.WriteLine("Digite o primeiro numero:");
             numero1 = int.Parse(Console.ReadLine());
@@ -18,8 +18,16 @@ namespace Calculadora
             numero2 = int.Parse(Console.ReadLine());
 
             soma = numero1 + numero2;
+            subtracao = numero1 - numero2;
+            divisao = numero1 / numero2;
+            multiplicacao = numero1 * numero2;
 
             Console.WriteLine($"A soma dos numeros é: {soma}");
+            Console.WriteLine($"A subtração dos numeros é: {subtracao}");
+            Console.WriteLine($"A divisão dos numeros é: {divisao}");
+            Console.WriteLine($"A multiplicação dos numeros é: {multiplicacao}");
+
+            Console.WriteLine("Digite enter para encerrar o programa");
             Console.ReadKey();
 
         }
